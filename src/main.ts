246b232d99fff1cdb7821e97b9bc881e47d38c1b
Aanand Prasad <aanand.prasad@gmail.com>
2026-08-16 15:13:52 -0400

    Copy helloTriangle example

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,83 @@
+import triangleVertWGSL from './shaders/triangle.vert.wgsl?raw';
+import redFragWGSL from './shaders/red.frag.wgsl?raw';
+
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-Hello world!
+<canvas></canvas>
 `
+
+// Adapted from https://github.com/webgpu/webgpu-samples/tree/main/sample/helloTriangle
+async function initialize() {
+  const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+  const adapter = await navigator.gpu?.requestAdapter({});
+  const device = await adapter?.requestDevice();
+  if (!device) {
+    throw new Error("Failed to request device");
+  }
+
+  const context = canvas.getContext('webgpu') as GPUCanvasContext;
+  if (!context) {
+    throw new Error("Failed to get canvas context");
+  }
+
+  const devicePixelRatio = window.devicePixelRatio;
+  canvas.width = canvas.clientWidth * devicePixelRatio;
+  canvas.height = canvas.clientHeight * devicePixelRatio;
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+  context.configure({
+    device,
+    format: presentationFormat,
+  });
+
+  const pipeline = device.createRenderPipeline({
+    layout: 'auto',
+    vertex: {
+      module: device.createShaderModule({
+        code: triangleVertWGSL,
+      }),
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: redFragWGSL,
+      }),
+      targets: [
+        {
+          format: presentationFormat,
+        },
+      ],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+  });
+
+  const frame = () => {
+    const commandEncoder = device.createCommandEncoder();
+    const textureView = context.getCurrentTexture().createView();
+
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+      colorAttachments: [
+        {
+          view: textureView,
+          clearValue: [0, 0, 0, 0], // Clear to transparent
+          loadOp: 'clear',
+          storeOp: 'store',
+        },
+      ],
+    };
+
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);
+    passEncoder.draw(3);
+    passEncoder.end();
+
+    device.queue.submit([commandEncoder.finish()]);
+    requestAnimationFrame(frame);
+  }
+
+  requestAnimationFrame(frame);
+}
+
+initialize();
