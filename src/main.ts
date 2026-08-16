80f108274e3260a5fd1f56c3eacf6d3b97cb501a
Aanand Prasad <aanand.prasad@gmail.com>
2026-08-16 16:41:11 -0400

    Replace helloTriangle with compute shader example

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-import triangleVertWGSL from './shaders/triangle.vert.wgsl?raw';
-import redFragWGSL from './shaders/red.frag.wgsl?raw';
-
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -21,63 +18,78 @@ async function initialize() {
     throw new Error("Failed to get canvas context");
   }
 
-  const devicePixelRatio = window.devicePixelRatio;
-  canvas.width = canvas.clientWidth * devicePixelRatio;
-  canvas.height = canvas.clientHeight * devicePixelRatio;
-  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+  const module = device.createShaderModule({
+    label: 'doubling compute module',
+    code: /* wgsl */`
+      @group(0) @binding(0) var<storage, read_write> data: array<f32>;
 
-  context.configure({
-    device,
-    format: presentationFormat,
+      @compute @workgroup_size(1) fn computeSomething(
+        @builtin(global_invocation_id) id: vec3u
+      ) {
+        let i = id.x;
+        data[i] = data[i] * 2.0;
+      }
+    `,
   });
 
-  const pipeline = device.createRenderPipeline({
+  const pipeline = device.createComputePipeline({
+    label: 'doubling compute pipeline',
     layout: 'auto',
-    vertex: {
-      module: device.createShaderModule({
-        code: triangleVertWGSL,
-      }),
-    },
-    fragment: {
-      module: device.createShaderModule({
-        code: redFragWGSL,
-      }),
-      targets: [
-        {
-          format: presentationFormat,
-        },
-      ],
-    },
-    primitive: {
-      topology: 'triangle-list',
+    compute: {
+      module,
     },
   });
 
-  const frame = () => {
-    const commandEncoder = device.createCommandEncoder();
-    const textureView = context.getCurrentTexture().createView();
+  const input = new Float32Array([1, 3, 5]);
 
-    const renderPassDescriptor: GPURenderPassDescriptor = {
-      colorAttachments: [
-        {
-          view: textureView,
-          clearValue: [0, 0, 0, 0], // Clear to transparent
-          loadOp: 'clear',
-          storeOp: 'store',
-        },
-      ],
-    };
+  const workBuffer = device.createBuffer({
+    label: 'work buffer',
+    size: input.byteLength,
+    // @ts-expect-error: GPUBufferUsage not defined
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST,
+  });
 
-    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-    passEncoder.setPipeline(pipeline);
-    passEncoder.draw(3);
-    passEncoder.end();
+  device.queue.writeBuffer(workBuffer, 0, input);
+  
+  const resultBuffer = device.createBuffer({
+    label: 'result buffer',
+    size: input.byteLength,
+    // @ts-expect-error: GPUBufferUsage not defined
+    usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+  });
 
-    device.queue.submit([commandEncoder.finish()]);
-    requestAnimationFrame(frame);
-  }
+  const bindGroup = device.createBindGroup({
+    label: 'bindGroup for work buffer',
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [
+      { binding: 0, resource: workBuffer },
+    ],
+  });
 
-  requestAnimationFrame(frame);
+  const encoder = device.createCommandEncoder({
+    label: 'doubling encoder',
+  });
+  const pass = encoder.beginComputePass({
+    label: 'doubling compute pass',
+  });
+  pass.setPipeline(pipeline);
+  pass.setBindGroup(0, bindGroup);
+  pass.dispatchWorkgroups(input.length);
+  pass.end();
+
+  encoder.copyBufferToBuffer(workBuffer, 0, resultBuffer, 0, resultBuffer.size);
+
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);
+
+  // @ts-expect-error: GPUMapMode not defined
+  await resultBuffer.mapAsync(GPUMapMode.READ);
+  const result = new Float32Array(resultBuffer.getMappedRange());
+
+  console.log('input', input);
+  console.log('result', result);
+
+  resultBuffer.unmap();
 }
 
 initialize();
