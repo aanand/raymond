57fc68dfaa35dba706baad946ae6e57cb8d2a79c
Aanand Prasad <aanand.prasad@gmail.com>
2026-08-22 18:49:29 -0400

    Use SSL in development so Mobile Safari permits WebGPU

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import typegpuPlugin from 'unplugin-typegpu/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   base: '/raymond',
-  plugins: [typegpuPlugin()],
+  plugins: [
+    typegpuPlugin(),
+    // Required for WebGPU to work on Mobile Safari in local dev
+    basicSsl(),
+  ],
+  server: {
+    https: true,
+  },
 });
