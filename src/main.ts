a87eb93c227d951a851a61af661ee867ba25bdf0
Aanand Prasad <aanand.prasad@gmail.com>
2026-08-22 18:37:13 -0400

    Bump resolution back up

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { rotateAxis } from './utils';
 import './style.css';
 
 const aspectRatio = 1;
-const imageWidth = 100;
+const imageWidth = 600;
 const samplesPerPixel = 2000;
 const samplesPerPass = 1;
 const maxBounceDepth = 5;
